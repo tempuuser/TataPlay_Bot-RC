4c0f2327639cb9d44d4931cc07775accdd957b58
tempuuser <anmoloppb12@gmail.com>
2025-02-14 22:02:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-api_id = 17193413
+api_id = 26649585
 
-api_hash = "5e50e6380252d9d487093bae4171bf91"
+api_hash = "588a3ea6fd01ae88bd2e10fed7d55b2c"
 
-bot_token = "6094200729:AAF7U2jM_T35Wb4nWdvPdndFNjtZ7oQeTTA"
+bot_token = "7784135648:AAFzRNk7paS0aH8UBknZ6mad6D0CTwnFIhs"
 
 GROUP_TAG = "RC"
 
-sudo_users = [1606667548]
+sudo_users = [6052303737]
 
 DL_DONE_MSG = """
 ✅ <b> {} Completed In </b> <code>{}</code>
